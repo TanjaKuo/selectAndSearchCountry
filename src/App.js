2d73b41b906d2d163a9f7d66ee30044b847513cc
TanyaK <pctanya.kuo@gmail.com>
2022-07-27 15:15:08 +1000

    select change to search and able to select and search countries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useRef, useState } from "react";
+import { colourOptions } from "./data";
+
+
+const List = ({text,setCountryName, setOpenOptions, openOptions, }) => {
+  
+    //const nameRef = useRef();
+    const checkRef = () => {
+    //console.log(nameRef.current.innerText);
+    //console.log('list text',text)
+    //setCountry(text)
+    setCountryName(text)
+    setOpenOptions(!openOptions)
+
+  }
+  //ref={nameRef}
+  return (
+    <li  className='selected' onClick={checkRef}  className="liOption">
+      {text}
+    </li>
+  );
+}
+
 
 function App() {
+  const [inputValue, setInputValue] = useState('Select')
+  //console.log('home inputValue', inputValue)
+
+  const [countryName, setCountryName] = useState("");
+  console.log("home countryName", countryName);
+  
+  const [openOptions,setOpenOptions] = useState(false)
+  //const [openOptionsAgain,setOpenOptionsAgain] = useState(false)
+
+  // const [country, setCountry] = useState('')
+//   console.log('country value', country);
+
+  //const handleSearchCountryName = (e) => setInputValue(e.target.value);
+  
+  //  const nameRef = useRef();
+  // console.log("nameRef", nameRef);
+  // const inputRef = useRef();
+  // console.log("nameRef", inputRef);
+
+  const handleClick = (e) => {
+    e.target.placeholder = "Search";
+    //setInputValue('Search')
+    setOpenOptions(!openOptions)
+    
+  };
+    const handleSearchCountryName = (e) => {
+    //setInputValue(e.target.value)
+    setCountryName(e.target.value)
+  };
+
+  const filterCountry = colourOptions.filter((co) => {
+    return co.name.official
+      .toLocaleLowerCase()
+      .includes(countryName.toLocaleLowerCase());
+  });
+
+  const showOptions = () => {
+    if(countryName !== '') 
+    console.log('heello again');
+  }  
+
+  // const handleChangeInput = (e) => {
+  //   // setCountryName(country)
+  //   // console.log('change input countryName', countryName)
+  //   //console.log("abc", nameRef.current.innerText);
+  //   //console.log("abcde", text);
+  //    //setInputValue(country)
+  // };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>1</h1>
+      <input
+        type="text"
+        placeholder="Select"
+        onChange={handleSearchCountryName}
+        onClick={handleClick}
+        //ref={inputRef}
+        className="inputContainer"
+        //defaultValue={inputValue || ''}
+       value={countryName || ''}
+      />
+      {openOptions && (
+      
+      <div className="optionContainer">
+      <ul className="ulOption">
+      {filterCountry.map((b) => (
+        <List key={b.name.official} 
+        text={b.name.official} 
+        //onClick={handleChangeInput} 
+        setCountryName={setCountryName} 
+        setOpenOptions={setOpenOptions} 
+        openOptions={openOptions}
+        // openAgagin={openAgagin}
+        // setOpenAgain={setOpenAgain}
+        />
+      ))}
+      </ul>
+      </div>) }
+
+
+
+      {/* {openOptionsAgain && (
+      <div className="optionContainer">
+        <h1>2</h1>
+      <ul className="ulOption">
+      {filterCountry.map((b) => (
+        <List key={b.name.official} 
+        text={b.name.official} 
+        //onClick={handleChangeInput} 
+        setCountryName={setCountryName} 
+        setOpenOptions={setOpenOptions} 
+        openOptions={openOptions}
+        // openAgagin={openAgagin}
+        // setOpenAgain={setOpenAgain}
+        />
+      ))}
+      </ul>
+      </div>) } */}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const colourOptions = [
+  { value: "ocean", name: { official: "Australia", code: "aud" } },
+  { value: "blue", name: { official: "Japan", code: "jp" } },
+  { value: "purple", name: { official: "Korea", code: "kr" } },
+  { value: "red", name: { official: " USA", code: "us" } },
+  { value: "orange", name: { official: "Vitenam", code: "vi" } },
+  //{ value: "yellow", name: { official: "Taiwan", code: "aud" } },
+];
