290ff93149201cbe213be295bffbe6aede1edcb1
TanyaK <pctanya.kuo@gmail.com>
2022-07-28 22:48:15 +1000

    install axios and fetch api and change font css

--- src/App.js
+++ src/App.js
@@ -1,125 +1,146 @@
-import logo from "./logo.svg";
 import "./App.css";
-import React, { useRef, useState } from "react";
-import { colourOptions } from "./data";
+import React, { useEffect, useId, useState } from "react";
+import axios from "axios";
+import { colourOptions, colourOptions2 } from "./data";
 
-
-const List = ({text,setCountryName, setOpenOptions, openOptions, }) => {
-  
-    //const nameRef = useRef();
-    const checkRef = () => {
+const List = ({ text, setCountryName, setOpenOptions, openOptions }) => {
+  //const nameRef = useRef();
+  const checkRef = () => {
     //console.log(nameRef.current.innerText);
     //console.log('list text',text)
     //setCountry(text)
-    setCountryName(text)
-    setOpenOptions(!openOptions)
-
-  }
+    setCountryName(text);
+    setOpenOptions(!openOptions);
+  };
   //ref={nameRef}
   return (
-    <li  className='selected' onClick={checkRef}  className="liOption">
+    <li onClick={checkRef} className="liOption">
       {text}
     </li>
   );
-}
-
+};
 
 function App() {
-  const [inputValue, setInputValue] = useState('Select')
-  //console.log('home inputValue', inputValue)
-
   const [countryName, setCountryName] = useState("");
   //console.log("home countryName", countryName);
-  
-  const [openOptions,setOpenOptions] = useState(false)
-  //const [openOptionsAgain,setOpenOptionsAgain] = useState(false)
+  const [countryInfo, setCountryInfo] = useState([]);
+  //console.log("countryInfo", countryInfo);
+  const [openOptions, setOpenOptions] = useState(false);
+  const [allCountry, setAllCountry] = useState(colourOptions);
+  //const [hasBackgroundColor, setHasBackgroundColor] = useState(false);
+  const id = useId();
+  const COUNTRY_URL = "https://restcountries.com/v3.1/all";
 
-  // const [country, setCountry] = useState('')
-//   console.log('country value', country);
+  // get index of country
+  const getIndex = countryInfo.map((co) => co.name === countryName);
+  //console.log("getIndex", getIndex);
 
-  //const handleSearchCountryName = (e) => setInputValue(e.target.value);
-  
-  //  const nameRef = useRef();
-  // console.log("nameRef", nameRef);
-  // const inputRef = useRef();
-  // console.log("nameRef", inputRef);
+  const getIndex2 = (countryName) => {
+    return countryInfo.findIndex((obj) => obj.name === countryName);
+  };
 
+  const currentCountryIndexToNumber = parseInt(getIndex2(countryName));
+  console.log("currentCountryIndexToNumber", currentCountryIndexToNumber);
+
+  //console.log(getIndex2(countryName));
+
+  // const getRightIndex = () => {
+  //   if (countryName && getIndex) {
+  //     console.log("this is the right one and contine what are you doing");
+  //   } else {
+  //     console.log("this is wrong, nothing here");
+  //   }
+  // };
+
+  const filterCountry = countryInfo.filter((co) => {
+    return co.name
+      .toLocaleLowerCase()
+      .includes(countryName.toLocaleLowerCase());
+  });
+  console.log("1.filter", filterCountry);
+
+  const showMore = () => {
+    var obj = {};
+    obj["id"] = ":rw:Greenland";
+    obj["name"] = "Greenland5678";
+    obj["flag"] = "🇬🇱";
+    console.log("obj", obj);
+    const neighbor = filterCountry.push(obj);
+    console.log("2.filter", filterCountry);
+    return neighbor;
+    //return neighbor;
+    //console.log("neibo", neighbor);
+  };
   const handleClick = (e) => {
     e.target.placeholder = "Search";
-    //setInputValue('Search')
-    setOpenOptions(!openOptions)
+    setOpenOptions(!openOptions);
+    // all -> country name , abc -> true
+    //const search = abc.map((c) => c === true);
+
+    //-----getRightIndex();
+
+    if (countryName) {
+      showMore();
+      console.log("show something??");
+      //const currentCountryIndex = colourOptions.findIndex(countryName);
+      //console.log("currentCountryIndex", currentCountryIndex);
+      // console.log("filterCountry", filterCountry);
+      // console.log("open countryName", countryName);
+
+      // console.log("countryName", countryName, getIndex2(countryName));
+      // console.log("countryName next", parseInt(getIndex2(countryName)));
+
+      //console.log("filterCountry", filterCountry.puch("Japan"));
+
+      // console.log(
+      //   "push",
+      //   filterCountry.push(getIndex2(countryName).length - 1)
+      // );
+    } else {
+      console.log("no one");
+    }
+    // console.log("colourOptions", colourOptions);
+    // console.log("countryName", countryName);
     // if(countryName !== '' && !filterCountry[1]) {
     //   console.log(" no more options filterCountry", filterCountry)
     // }
     //showMoreOption()
+    // if (e.target.value !== "") {
+    //   e.target.placeholder = countryName;
+    //   e.target.value = "";
+    //   console.log("1. e.target.value", e.target.value);
+    // }
+
+    // if (countryName !== "") {
+    //   e.target.placeholder = countryName;
+    //   e.target.value = "";
+    //   console.log("e.target.placeholder", e.target.placeholder);
+    //   console.log("2. e.target.value", e.target.value);
+    // }
   };
-    const handleSearchCountryName = (e) => {
+
+  const handleSearchCountryName = (e) => {
     //setInputValue(e.target.value)
-    setCountryName(e.target.value)
+    setCountryName(e.target.value);
+    // if (countryName) {
+    //   setAllCountry(filterCountry);
+    // }
   };
 
-  const filterCountry = colourOptions.filter((co) => {
-    return co.name.official
-      .toLocaleLowerCase()
-      .includes(countryName.toLocaleLowerCase());
-  });
-  console.log('1.filter', filterCountry);
-
-  
-const currentCountryIndex = colourOptions.find((co) => co.name.official === filterCountry[0].name.official )
-console.log('curret', currentCountryIndex);
-console.log('curret', currentCountryIndex);
-
-
-  // const showMoreOption = () => {
-
-  //  // define current country index
-  // const currentCountry = filterCountry[0].name.official
-  // console.log('showmore currentCountry', currentCountry);
-  
-  // // map every
-  // const eachCountry = colourOptions.map(co => co.name.official)
-  
-  // eachCountry.forEach((co, i) => console.log('co, i', co, i))
-
-  // // find the index
-  // for (let i = 0; i< colourOptions.length; i++) {
-  //   const currentIndex = 
-  //   if(currentCountry === eachCountry[i]) {
-  //     console.log('found it')
-  //   } else {
-  //     console.log('no')
-  //   }
-  // }
-  
-
-    // for (let i = 0; i < filterCountry.length; i++) {
-    //   if(currentCountry !== filterCountry[0].name.official) {
-    //     console.log('no found!')
-    //   } else {
-    //     console.log('found it')
-    //   }
-    // }
-    // const otherOptions = colourOptions.map((c) => {
-    //   return c.name.official
-    // })
-
-    // console.log('otherOptions', otherOptions);
-    // console.log('filter inside', filterCountry);
-    // if(currentCountry) {
-    //   console.log(currentCountry.length)
-    // //return filterCountry.push(otherOptions[0], otherOptions[1], currentCountry, otherOptions[3], otherOptions[4])
-    // }
-    // return filterCountry
-    //console.log('showmore otherOption', otherOptions);
-  //}
-
-
-  // const showOptions = () => {
-  //   if(countryName !== '') 
-  //   console.log('heello again');
-  // }  
-
+  useEffect(() => {
+    const getCountryData = async () => {
+      const response = await axios.get(COUNTRY_URL);
+      const datas = response.data;
+      const eachCountryInfo = datas.map((data) => ({
+        id: id + data.name.official,
+        name: data.name.official,
+        flag: data.flag,
+      }));
+      //console.log("eachInfo", eachInfo);
+      setCountryInfo(eachCountryInfo);
+    };
+    getCountryData();
+  }, [id]);
 
   return (
     <div className="App">
@@ -129,47 +150,28 @@ console.log('curret', currentCountryIndex);
         placeholder="Select"
         onChange={handleSearchCountryName}
         onClick={handleClick}
-        //ref={inputRef}
         className="inputContainer"
         //defaultValue={inputValue || ''}
-       value={countryName || ''}
+        value={countryName || ""}
       />
       {openOptions && (
-      <div className="optionContainer">
-      <ul className="ulOption">
-      {filterCountry.map((b) => (
-        <List key={b.name.official} 
-        text={b.name.official} 
-        //onClick={handleChangeInput} 
-        setCountryName={setCountryName} 
-        setOpenOptions={setOpenOptions} 
-        openOptions={openOptions}
-        // openAgagin={openAgagin}
-        // setOpenAgain={setOpenAgain}
-        />
-      ))}
-      </ul>
-      </div>) }
-
-
-
-      {/* {openOptionsAgain && (
-      <div className="optionContainer">
-        <h1>2</h1>
-      <ul className="ulOption">
-      {filterCountry.map((b) => (
-        <List key={b.name.official} 
-        text={b.name.official} 
-        //onClick={handleChangeInput} 
-        setCountryName={setCountryName} 
-        setOpenOptions={setOpenOptions} 
-        openOptions={openOptions}
-        // openAgagin={openAgagin}
-        // setOpenAgain={setOpenAgain}
-        />
-      ))}
-      </ul>
-      </div>) } */}
+        <div className="optionContainer">
+          <ul className="ulOption">
+            {filterCountry.map((b) => (
+              <List
+                key={b.name}
+                text={b.name}
+                setCountryName={setCountryName}
+                setOpenOptions={setOpenOptions}
+                openOptions={openOptions}
+                // style={{
+                //   backgroundColor: hasBackgroundColor ? "red" : "green",
+                // }}
+              />
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
